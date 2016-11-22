35bbb87e1b62014201b84bec7d1864be3478a6cc
VeryBarry <barry.a.daniels@gmail.com>
2016-11-22 11:16:00 -0500

    Updated controller

--- src/main/java/com/theironyard/controllers/HannibalLecturerController.java
+++ src/main/java/com/theironyard/controllers/HannibalLecturerController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -18,7 +19,7 @@ import java.sql.SQLException;
 /**
  * Created by VeryBarry on 10/26/16.
  */
-@Controller
+@RestController
 public class HannibalLecturerController {
     @Autowired
     LecturerRepository lecturers;
@@ -39,20 +40,20 @@ public class HannibalLecturerController {
     }
 
     @RequestMapping(path = "/lecturers", method = RequestMethod.POST)
-    public String createLecturer(String name, String topic, String image) {
+    public void createLecturer(String name, String topic, String image) {
         Lecturer lecturer = new Lecturer(name, topic, image);
         lecturers.save(lecturer);
-        return "index";
     }
     @RequestMapping(path = "/lecturers", method = RequestMethod.GET)
     public Iterable<Lecturer> getLectures () {
         return lecturers.findAll();
     }
+
+
     @RequestMapping(path = "/reviews", method = RequestMethod.POST)
-    public String createReview(String author, String text, boolean bool) {
+    public void createReview(String author, String text, boolean bool) {
         Review review = new Review (author, text, bool);
         reviews.save(review);
-        return "index";
     }
     @RequestMapping(path = "/reviews", method = RequestMethod.GET)
     public Iterable<Review> getReviews () {
